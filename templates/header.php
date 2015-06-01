494b3117cfceba8aef391119c75ea501472ea63c
cjwd <Pr@1seH!m>
2015-06-01 15:25:22 -0400

    According to W3C spec h2 tags are only for subheading of page sections

--- templates/header.php
+++ templates/header.php
@@ -3,14 +3,14 @@
 <header id="masthead" class="site-header site-header--masthead clearfix" role="banner">
   <div class="site-branding">
     <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-    <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+    <p class="site-description"><?php bloginfo( 'description' ); ?></p>
   </div><!-- .site-branding -->
 
   <nav id="site-navigation" class="main-navigation" role="navigation">
     <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'spring' ); ?></button>
-    <?php 
+    <?php
     if (has_nav_menu('primary_navigation')) :
-      wp_nav_menu( ['theme_location' => 'primary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'primary_navigation', 'depth' => 3] ); 
+      wp_nav_menu( ['theme_location' => 'primary_navigation', 'walker' => new NavWalker(), 'menu_class' => 'primary_navigation', 'depth' => 3] );
     endif;
     ?>
   </nav><!-- #site-navigation -->
